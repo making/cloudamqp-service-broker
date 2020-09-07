9826fc32dd9587ed55bcebef0b1ce010e5372c27
Toshiaki Maki <tmaki@pivotal.io>
2020-09-07 23:26:59 +0900

    Fix

--- src/main/java/am/ik/servicebroker/cloudamqp/config/SecurityConfig.java
+++ src/main/java/am/ik/servicebroker/cloudamqp/config/SecurityConfig.java
@@ -2,30 +2,21 @@ package am.ik.servicebroker.cloudamqp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
-@Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
-    private final ServiceBrokerAdmin admin;
 
-    public SecurityConfig(ServiceBrokerAdmin admin) {
-        this.admin = admin;
-    }
-
-    @Bean
-    public SecurityWebFilterChain springWebFilterChain(ServerHttpSecurity http) throws Exception {
-        return http
+	@Bean
+	public SecurityWebFilterChain springWebFilterChain(ServerHttpSecurity http) throws Exception {
+		return http
 				.authorizeExchange()
-                .pathMatchers("/v2/**").hasRole("ADMIN") //
-                .pathMatchers("/application/**").hasRole("ADMIN")
+				.pathMatchers("/v2/**").hasRole("ADMIN") //
+				.and()
+				.httpBasic()
 				.and()
 				.build();
-    }
-
-    @Bean
-    public MapReactiveUserDetailsService userDetailsRepository() {
-        return new MapReactiveUserDetailsService(this.admin);
-    }
+	}
 }
